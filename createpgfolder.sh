baa73408cf072eaa5ee41df4d98d9387b792fd29
IAmTheLime <a70672@alunos.uminho.pt>
2017-01-22 16:15:54 +0000

    Mais perto de transacionar

---
+++ createpgfolder.sh
@@ -0,0 +1,1 @@
+sudo mkdir /run/postgresql;sudo chmod 777 /run/postgresql;
--- src/main/java/bolsa/Settlement.java
+++ src/main/java/bolsa/Settlement.java
@@ -4,8 +4,15 @@ package bolsa;
 import com.google.protobuf.CodedInputStream;
 import org.zeromq.ZMQ;
 
+import javax.jms.*;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+import javax.transaction.UserTransaction;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
+import java.sql.Connection;
+import java.sql.Statement;
 
 
 /**
@@ -26,6 +33,29 @@ public class Settlement {
             while(true){
                 String aux = subscriber.recvStr(Charset.defaultCharset());
                 System.out.println(aux);
+
+                Context ctx = new InitialContext();
+
+                UserTransaction txn = (UserTransaction) ctx.lookup("java:comp/UserTransaction");
+
+                txn.begin();
+
+                ConnectionFactory cf = (ConnectionFactory) ctx.lookup("jms/settlementConnection");
+                javax.jms.Connection c2 = cf.createConnection();
+                Session s = c2.createSession(false, 0);
+                Queue q = s.createQueue("FILA1");
+                MessageProducer p = s.createProducer(q);
+
+                TextMessage m = s.createTextMessage(aux);
+                m.setIntProperty("price", 10);
+                p.send(m);
+
+                p.close();
+                s.close();
+                c2.close();
+
+                txn.commit();
+
 
             }
 
