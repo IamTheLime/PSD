c48e87c5c29b990bde9ace2c57403a10005a1141
IAmTheLime <a70672@alunos.uminho.pt>
2017-01-22 01:30:30 +0000

    Mais merda

--- src/main/java/bolsa/Bank.java
+++ src/main/java/bolsa/Bank.java
@@ -1,7 +1,30 @@
 package bolsa;
 
+import javax.jms.*;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.transaction.UserTransaction;
+
 /**
  * Created by tiago on 1/21/17.
  */
 public class Bank {
+
+
+    public static void main(String[] args) throws Exception{
+        Context ctx = new InitialContext();
+        UserTransaction txn = (UserTransaction) ctx.lookup("java:comp/UserTransaction");
+
+        txn.begin();
+        ConnectionFactory cf = (ConnectionFactory) ctx.lookup("jms/bankConnection");
+        javax.jms.Connection c1 = cf.createConnection();
+        c1.start();
+        Session s = c1.createSession(false,0);
+        Topic q = s.createTopic("TransactionToSettlement");
+        MessageConsumer mc = s.createDurableSubscriber(q,"teste");
+        ObjectMessage obj = (ObjectMessage) mc.receive();
+        System.out.println(((Transacao.Transaction)obj.getObject()).getEmpresa());
+        txn.commit();
+
+    }
 }
--- src/main/java/bolsa/Settlement.java
+++ src/main/java/bolsa/Settlement.java
@@ -1,10 +1,13 @@
 package bolsa;
 
+import jdk.nashorn.internal.ir.CatchNode;
+
 import javax.jms.*;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
 import javax.transaction.UserTransaction;
+import java.lang.reflect.Executable;
 import java.sql.Connection;
 import java.sql.Statement;
 /**
@@ -12,20 +15,22 @@ import java.sql.Statement;
  */
 public class Settlement {
 
-    public static void main(String[] args) throws Exception{
-        Context ctx = new InitialContext();
-        UserTransaction txn = (UserTransaction) ctx.lookup("java:comp/UserTransaction");
+    public static void main(String[] args) {
+        try {
+            Context ctx = new InitialContext();
+            UserTransaction txn = (UserTransaction) ctx.lookup("java:comp/UserTransaction");
 
-        txn.begin();
-        ConnectionFactory cf = (ConnectionFactory) ctx.lookup("jms/settlementConnection");
-        javax.jms.Connection c1 = cf.createConnection();
-        c1.start();
-        Session s = c1.createSession(false,0);
-        Topic q = s.createTopic("TransactionToSettlement");
-        MessageConsumer mc = s.createDurableSubscriber(q,"teste");
-        ObjectMessage obj = (ObjectMessage) mc.receive();
-        System.out.println(((Transacao.Transaction)obj.getObject()).getEmpresa());
-        txn.commit();
-
+            txn.begin();
+            ConnectionFactory cf = (ConnectionFactory) ctx.lookup("jms/settlementConnection");
+            javax.jms.Connection c1 = cf.createConnection();
+            c1.start();
+            Session s = c1.createSession(false, 0);
+            Topic q = s.createTopic("TransactionToSettlement");
+            MessageConsumer mc = s.createDurableSubscriber(q, "teste");
+            ObjectMessage obj = (ObjectMessage) mc.receive();
+            System.out.println(((Transacao.Transaction) obj.getObject()).getEmpresa());
+            txn.commit();
+        }
+        catch (Exception e){e.printStackTrace();}
     }
 }
